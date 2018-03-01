79cc2da77b485bfa36b292c508d9806e2c8e8a7d
Nolan Emirot <nolan@turo.com>
2018-02-28 16:12:49 -0800

    Create test.r

---
+++ test.r
@@ -0,0 +1,6 @@
+library(downloader)
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/msleep_ggplot2.csv"
+filename <- "msleep_ggplot2.csv"
+if (!file.exists(filename)) download(url,filename)
+msleep <- read.csv("msleep_ggplot2.csv")
+head(msleep)
